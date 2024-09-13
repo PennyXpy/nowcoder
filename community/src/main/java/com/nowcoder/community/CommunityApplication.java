c10b54891c6c4d472454537d6a253790258a60a1
Pingyi Xu <xu.pin@northeastern.edu>
2024-09-13 03:15:42 -0700

    test bean

--- community/src/main/java/com/nowcoder/community/CommunityApplication.java
+++ community/src/main/java/com/nowcoder/community/CommunityApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+// 注解很有必要 - 没有注解 就不可以实现其他功能也就是SpringBootApplication上面注解的功能
 public class CommunityApplication {
 
 	public static void main(String[] args) {
--- community/src/test/java/com/nowcoder/community/CommunityApplicationTests.java
+++ community/src/test/java/com/nowcoder/community/CommunityApplicationTests.java
@@ -1,13 +1,31 @@
 package com.nowcoder.community;
 
+import com.nowcoder.community.dao.AlphaDao;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.BeansException;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class CommunityApplicationTests {
+@ContextConfiguration(classes = CommunityApplication.class)
+class CommunityApplicationTests implements ApplicationContextAware {
 
-	@Test
-	void contextLoads() {
+	private ApplicationContext applicationContext;
+
+	@Override
+	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+		this.applicationContext = applicationContext;
 	}
+	@Test
+	void testApplicationContext() {
+		System.out.println(applicationContext);
 
+		AlphaDao alphaDao =  applicationContext.getBean(AlphaDao.class);
+		System.out.println(alphaDao.select());
+	}
 }
